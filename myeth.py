3fcea4da87ce303156e4d2701856b24df7b63ab7
Ifiri800 <eopumie@gmail.com>
2021-10-12 18:14:52 +0100

    my second commit

--- myeth.py
+++ myeth.py
@@ -1,23 +1,37 @@
 from web3 import Web3
+from web3.contract import parse_block_identifier_int
+from web3.types import BlockData
+from datetime import datetime
+from time import timezone
 
 # Link the project and connecting to Infura
-eth_node = ""
+eth_node = "https://mainnet.infura.io/v3/84d9e7fb419a4fd1b74b758bf07c22ba"
 
 web3 = Web3(Web3.HTTPProvider(eth_node))
 print(web3.isConnected())
+print(web3.eth.blockNumber)
 
-def get_block():
-    block = web3.eth.get_block('latest')
-    return print(block)
 
-def get_block_number():
-    block_number = web3.eth.get_block_number()
-    print(block_number)
+accounts = web3.eth.get_accounts()
+test_address = "0xd757fd54b273BB1234d4d9993f27699d28d0EDD2"
+balance = web3.eth.get_balance(test_address)
+print("balance", web3.fromWei(balance, "ether"))
 
-get_block = web3.eth.get_block_number
-if block_number != 0:
-    block_info = web3.eth_get_block_number(block).get('hash').hex()
-    print(block_info)
+block = web3.eth.get_block('latest')
+print(block)
+
+
+block_data = web3.eth.get_transaction_by_block
+print(block_data)
+
+receipt = web3.eth.get_transaction_receipt
+print(receipt)
+
+transaction = web3.eth._get_transaction
+print(transaction)
+
+block_info = web3.eth.get_transaction_by_block
+print(block_info)
 
 
 
