d97d0648b8b73c7f0d974868fa213632fa2d5523
Ifiri800 <eopumie@gmail.com>
2021-10-12 10:39:12 +0100

    This is my second commit

--- myeth.py
+++ myeth.py
@@ -1,1 +1,23 @@
 from web3 import Web3
+
+# Link the project and connecting to Infura
+eth_node = ""
+
+web3 = Web3(Web3.HTTPProvider(eth_node))
+print(web3.isConnected())
+
+def get_block():
+    block = web3.eth.get_block('latest')
+    return print(block)
+
+def get_block_number():
+    block_number = web3.eth.get_block_number()
+    print(block_number)
+
+get_block = web3.eth.get_block_number
+if block_number != 0:
+    block_info = web3.eth_get_block_number(block).get('hash').hex()
+    print(block_info)
+
+
+
